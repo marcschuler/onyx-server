eacaa9b37efe2b2854cc46887a94c8fa210fdff6
Marc Schuler <karlthebee@googlemail.com>
2026-07-26 16:44:44 +0200

    Fixed tests error because no server is running

--- src/main/java/de/marcschuler/webrtcserver/config/WebSockerConfig.java
+++ src/main/java/de/marcschuler/webrtcserver/config/WebSockerConfig.java
@@ -4,6 +4,7 @@ import de.marcschuler.webrtcserver.service.websocket.WebSocketConnectionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
@@ -21,6 +22,7 @@ public class WebSockerConfig implements WebSocketConfigurer{
     }
 
     @Bean
+    @Profile("!test")
     public ServletServerContainerFactoryBean createServletServerContainerFactoryBean() {
         ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
         container.setMaxTextMessageBufferSize(32768);
